e538fa47d427c3b9de8c4a83a0ae5346dcffee0d
mark <vladys.marchenko@gmail.com>
2023-12-07 15:06:13 +0200

    useRef

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { Component, useEffect, useState, useCallback, useMemo } from 'react';
+import { Component, useEffect, useState, useCallback, useMemo, useRef } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
-import styled from 'styled-components';
 
 
 // class Slider extends Component {
@@ -137,9 +136,46 @@ const Slide = ({ getSomeImages }) => {
     )
 }
 
+
+// useRef
+const Form = () => {
+    const [text, setText] = useState('');
+
+    const myRef = useRef(1);
+
+    // const focusFirstTI = () => {
+    //     myRef.current.focus();
+    // }
+
+    useEffect(() => {
+        myRef.current++
+        console.log(myRef.current);
+    })
+
+    return (
+        <Container>
+            <form className="w-50 border mt-5 p-3 m-auto">
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                    <input onChange={(e) => setText(e.target.value)} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+            </form>
+        </Container>
+    )
+}
+
+
+
 function App() {
     return (
-        <Slider />
+        <>
+            <Form />
+            <Slider />
+        </>
     );
 }
 
